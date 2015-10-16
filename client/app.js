43f83b6bf678abf394b9eac631e2953ea5b96303
Tristan Rice <rice@fn.lc>
2015-10-16 22:38:03 +0000

    Updated to support HTTPS for login and fonts.

--- client/app.js
+++ client/app.js
@@ -1,3 +1,8 @@
+// Enable OAuth working with https urls.
+if (window.__meteor_runtime_config__) {
+  window.__meteor_runtime_config__.ROOT_URL = window.location.protocol + "//" +window.location.hostname;
+}
+
 Tickets = new Meteor.Collection("tickets");
 Announcements = new Meteor.Collection("announcements");
 Settings = new Meteor.Collection("settings");
